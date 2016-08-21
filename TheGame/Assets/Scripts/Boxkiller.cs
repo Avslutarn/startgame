dd97fe65443c182c9105009b088642478234a3f6
Avslutarn <avslutarn@gmail.com>
2016-08-21 17:53:00 +0200

    La till en scriptfil för att skjuta sönder lådjäveln!

---
+++ TheGame/Assets/Scripts/Boxkiller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boxkiller : MonoBehaviour {
+
+
+    public int reloadTime;
+    private bool reloading;
+
+	// Use this for initialization
+	void Start () {
+        reloading = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit);
+            if (hit.collider.tag == "spelkub")
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+	}
+}
