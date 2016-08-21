c7b8e8fb7649a62e654ac984fffb9e07c98ca191
Melburn <oscar.bringsater@gmail.com>
2016-08-21 16:21:29 +0200

    adding a jump script and adding it to the prefab

---
+++ TheGame/Assets/Jump.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour
+{
+
+    private bool hasJumped = false;
+    private System.Diagnostics.Stopwatch elapsedTime;
+    private Rigidbody rb;
+    public int minWaitTime;
+    public int maxWaitTime;
+    public int jumpForce;
+
+    // Use this for initialization
+    void Start()
+    {
+        elapsedTime = new System.Diagnostics.Stopwatch();
+        elapsedTime.Start();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasJumped && elapsedTime.ElapsedMilliseconds > minWaitTime)
+        {
+            Debug.Log("not jumped");
+            if(Random.value > 0.99f || elapsedTime.ElapsedMilliseconds > maxWaitTime)
+            {
+                Debug.Log("jump!");
+                rb.AddForce(Vector3.up * jumpForce);
+                hasJumped = true;
+            }
+        }
+    }
+}
