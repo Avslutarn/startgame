39281ba933b6f7b339bb408dcc2be1b27e5108b8
Avslutarn <avslutarn@gmail.com>
2016-08-22 20:16:15 +0200

    Lade till mad_rocket!

---
+++ TheGame/Assets/Scripts/RocketRider/RocketController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketController : MonoBehaviour {
+
+    public int roll;
+    public int elevation;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.right * elevation);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.left * elevation);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * roll);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * roll);
+        }
+    }
+}
---
+++ TheGame/Assets/Scripts/RocketRider/RocketEngine.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketEngine : MonoBehaviour {
+
+    private bool firing;
+    public Rigidbody rb;
+    public float RocketPower;
+    
+	// Use this for initialization
+	void Start () {
+        firing = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.forward * RocketPower);
+        }
+
+    }
+}
