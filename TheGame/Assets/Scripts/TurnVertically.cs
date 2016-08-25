b18487c396a14e42cc9f61cd836833b47840ca97
Melburn <oscar.bringsater@gmail.com>
2016-08-25 20:16:12 +0200

    adding movement to turret

---
+++ TheGame/Assets/Scripts/TurnVertically.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnVertically : MonoBehaviour
+{
+
+    public float speed;
+    Transform trans;
+    // Use this for initialization
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trans.Rotate(Vector3.left * Input.GetAxis("Mouse Y") * Time.deltaTime * speed);
+        Debug.Log(trans.eulerAngles);
+        if (trans.eulerAngles.x < 50)
+            trans.eulerAngles = new Vector3(360, trans.eulerAngles.y, trans.eulerAngles.z);
+        else if(trans.eulerAngles.x < 335)
+            trans.eulerAngles = new Vector3(335, trans.eulerAngles.y, trans.eulerAngles.z);
+    }
+}
