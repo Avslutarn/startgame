dd4a0fc34894540089eeec89537becd54beba60d
Avslutarn <avslutarn@gmail.com>
2016-08-25 20:35:53 +0200

    lagt till ankjävlar

---
+++ TheGame/Assets/objekt/TheTurret/DuckBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckBehaviour : MonoBehaviour {
+
+    public float flyspeed;
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.right * Time.deltaTime * flyspeed;
+
+
+	}
+}
---
+++ TheGame/Assets/objekt/TheTurret/DuckSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckSpawner : MonoBehaviour {
+
+    public GameObject duckfab;
+    public float spawnRate = 3f;
+    public Transform[] spawnPoints;
+
+
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", spawnRate, spawnRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Spawn ()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(duckfab, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
