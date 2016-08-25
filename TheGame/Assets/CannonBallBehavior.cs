ec748835804195922a49d3957e8cee798ac0fde0
Elof Bigestans <Elof Bigestans>
2016-08-25 21:07:04 +0200

    Added cannonball components

---
+++ TheGame/Assets/CannonBallBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBallBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collision!");
+        if (col.gameObject.name == "DaDuck")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ TheGame/Assets/Scripts/CannonBallController/CreateCannonBall.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createCannonBall : MonoBehaviour {
+
+    public Rigidbody ball;
+    public float force = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            FireBall();
+        }
+	
+	}
+
+    void FireBall()
+    {
+        GameObject gunNeck2 = GameObject.Find("GunNeck2");
+        Vector3 gunNeck2Front = gunNeck2.transform.position;
+        Debug.Log(gunNeck2Front);
+        Debug.Log(gunNeck2.);
+        Rigidbody ballClone = (Rigidbody) Instantiate(ball, gunNeck2Front, gunNeck2.transform.rotation);
+        ballClone.AddForce(gunNeck2.transform.position * force);
+    }
+}
