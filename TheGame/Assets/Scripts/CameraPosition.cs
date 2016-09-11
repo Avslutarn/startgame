25d18ec965488518af8e03a13e222de355aea510
Melburn <oscar.bringsater@gmail.com>
2016-09-11 10:59:31 +0200

    adding a script for Camera Position, and player prefab

---
+++ TheGame/Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraPosition : MonoBehaviour {
+
+    Transform transform;
+    Transform playerTrans;
+	// Use this for initialization
+	void Start () {
+        transform = GetComponent<Transform>();
+        playerTrans = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playerTrans.position + new Vector3(4, 2, 0);
+	}
+}
