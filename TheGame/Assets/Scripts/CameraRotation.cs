40a9a16169dfb88ec9fa94211debe8d8b26aebfe
Melburn <oscar.bringsater@gmail.com>
2016-09-11 11:33:54 +0200

    adding camera-rotation

---
+++ TheGame/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+    public float speed;
+    Transform trans;
+    // Use this for initialization
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trans.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * speed, Space.World);
+        trans.Rotate(Vector3.left * Input.GetAxis("Mouse Y") * Time.deltaTime * speed);
+    }
+}
